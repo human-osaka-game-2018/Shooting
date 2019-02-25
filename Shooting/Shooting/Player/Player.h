60ebff731556d9bbc19bba856d7765694efed1fd
yahara-mitsuaki <yaharamitsuaki4@gmail.com>
2019-02-25 18:00:16 +0900

    [issue #7] 弾の描画

--- Shooting/Shooting/Player/Player.h
+++ Shooting/Shooting/Player/Player.h
@@ -7,31 +7,16 @@ class Player
 public:
 	Player();
 
-	inline float getPosX()
+	inline POSITION getPos()
 	{
-		return m_Pos.x;
-	}
-
-	inline float getPosY()
-	{
-		return m_Pos.y;
-	}
-
-	inline void setPosX(float x)
-	{
-		m_Pos.x = x;
-	}
-
-	inline void setPosY(float y)
-	{
-		m_Pos.y = y;
+		return m_Pos;
 	}
 
 	void control(BYTE* diks);
-	void render(IDirect3DDevice9* g_pD3Device, IDirect3DTexture9* g_pTexture);
+	void render(IDirect3DDevice9* g_pD3Device, IDirect3DTexture9** g_pTexture);
 private:
 	POSITION m_Pos;
-	const POSITION m_StartPos = { 150.f ,400.f, 50.f };
+	const POSITION m_StartPos = { 150.f ,400.f, 50.f, 50.f };
 	const float m_MovmentAmount = 4;
 	int status;
 	int counter;
